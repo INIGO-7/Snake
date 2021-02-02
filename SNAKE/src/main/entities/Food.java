d0d6845a44721c94d63ed6875a2a98dc1f463a65
Iñigo Fernandez <inigofernandez@opendeusto.es>
2021-02-02 20:30:05 +0100

    Food gets generated properly and snake moves without error.

--- SNAKE/src/main/entities/Food.java
+++ SNAKE/src/main/entities/Food.java
@@ -2,11 +2,30 @@ package main.entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Food {
 
+	private int x, y, width = 24, height = 24, nextX, nextY;
+	private Random xVal = new Random(), yVal = new Random();
+	
 	public Food() {
+		foodPositionGenerator();
+	}
+	
+	public void foodPositionGenerator() {
+		nextX = xVal.nextInt(776) + 1;
+		nextY = yVal.nextInt(576) + 1;
 		
+		while((nextX - 1) % 50 != 0 || (nextX - 1) % 25 != 0) nextX++;
+		while((nextY - 1) % 50 != 0 || (nextY - 1) % 25 != 0) nextY++;
+		
+		//////////////////////////////////////////////////////////////////////////
+		///////	check that the food doesn't appear in the middle of snake: ///////
+		//////////////////////////////////////////////////////////////////////////
+		
+		x = nextX;
+		y = nextY;
 	}
 	
 	public void tick() {
@@ -15,7 +34,7 @@ public class Food {
 	
 	public void render(Graphics g) {
 		g.setColor(Color.RED);
-		g.fillRect(201, 201, 24, 24);
+		g.fillRect(x, y, width, height);
 	}
 	
 }
--- SNAKE/src/main/entities/Snake.java
+++ SNAKE/src/main/entities/Snake.java
@@ -2,24 +2,50 @@ package main.entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Snake {
 
+	private LinkedList<Rectangle> snakeBody;
+	private long current, last;
+	private double diff, rate = 0.2;
+	
 	public Snake() {
+		snakeBody = new LinkedList<Rectangle>();
+		snakeBody.add(new Rectangle(301, 301, 24, 24));
+		snakeBody.add(new Rectangle(301, 326, 24, 24));
+		snakeBody.add(new Rectangle(301, 351, 24, 24));
 		
+		last = System.nanoTime();
 	}
 	
 	public void tick() {
 		
+		current = System.nanoTime();
+		diff += current - last;
+		
+		if(diff/1000000000 >= rate) {
+			
+			Rectangle lastRect = snakeBody.getLast();
+			lastRect.y = snakeBody.getFirst().y - 25;
+			snakeBody.removeLast();
+			snakeBody.addFirst(lastRect);
+			
+			diff = 0;
+		}
+		
+		last = current;
+		
 	}
 	
 	public void render(Graphics g) {
+		
 		g.setColor(Color.GREEN);
-		g.fillRect(301, 301, 24, 24);
-		g.fillRect(301, 326, 24, 24);
-		g.fillRect(301, 351, 24, 24);
-		g.fillRect(301, 376, 24, 24);
-		g.fillRect(301, 401, 24, 24);
+		for(Rectangle rect : snakeBody) {
+			g.fillRect(rect.x, rect.y, rect.width, rect.height);
+		}
 	}
 	
 }
